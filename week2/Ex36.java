4b9cb2c31aa629453ee4c8c15f66246f2688dfae
Annely <annely.kurg@khk.ee>
2022-09-08 12:38:50 +0300

    week 2 - ex 36.5

--- week2/Ex36.java
+++ week2/Ex36.java
@@ -10,6 +10,8 @@ public class Ex36 {
         int number;
         int sum = 0;
         int count = 0;
+        int even = 0;
+        int odd = 0;
         while (true) {
             number = Integer.parseInt(reader.nextLine());
             if (number ==-1) {
@@ -17,12 +19,19 @@ public class Ex36 {
             }
             sum += number; //sum = sum + number
             count ++;
+            if(number % 2 == 0) {
+                even ++;
+            } else {
+                odd ++;
+            }
         }
         System.out.println("Thank you and see you later!");
         System.out.println("The sum is " + sum);
         System.out.println("How many numbers: " + count);
         double average = (double)sum / count;
         System.out.println("Average: " + average); //average = sum/count
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
     }
 
 }
