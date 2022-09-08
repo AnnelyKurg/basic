b83de2840dcb3f3e1704e6f04afa17d3bc5f837e
Annely <annely.kurg@khk.ee>
2022-09-08 14:02:56 +0300

    week 2 - Exercise 39.3 printRectangle

--- week2/Ex39.java
+++ week2/Ex39.java
@@ -21,12 +21,25 @@ public class Ex39 {
             row++;
         }
     }
+    private static void printRectangle(int width, int height){
+        int row = 1;
+        while(row <= height){
+            int field = 1;
+            while(field <= width){
+                System.out.print("* ");
+                field++;
+            }
+            System.out.println();
+            row++;
+        }
+    }
 
     public static void main(String[] args) {
         printStars(5);
         printStars(3);
         printStars(9);
         printSquare(4);
+        printRectangle(17,3);
 
     }
 
