8a66462b619c9cd7e59b0f6ec2b25b6e7aa83afb
Annely <annely.kurg@khk.ee>
2022-09-08 12:25:31 +0300

    week 2 - ex 36.2

--- week2/Ex36.java
+++ week2/Ex36.java
@@ -8,13 +8,16 @@ public class Ex36 {
         System.out.println("Type number: ");
         Scanner reader = new Scanner(System.in);
         int number;
+        int sum = 0;
         while (true) {
             number = Integer.parseInt(reader.nextLine());
             if (number ==-1) {
                 break;
             }
+            sum += number; //sum = sum + number
         }
         System.out.println("Thank you and see you later!");
+        System.out.println("The sum is " + sum);
     }
 
 }
