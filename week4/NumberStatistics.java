e12209a0050e4299a7b3de1f3b9a39dc5e6badea
Annely <annely.kurg@khk.ee>
2022-09-09 11:27:03 +0300

    week 4 - Exercise 79.1

---
+++ week4/NumberStatistics.java
@@ -0,0 +1,21 @@
+package week4;
+
+public class NumberStatistics {
+    private int amountOfNumbers;
+
+    public NumberStatistics() {
+        // initialize here the object variable amountOfNumbers
+        this.amountOfNumbers = amountOfNumbers;
+    }
+
+    public void addNumber(int number) {
+        // code here
+        this.amountOfNumbers = number;
+    }
+
+    public int amountOfNumbers() {
+        // code here
+        return this.amountOfNumbers + amountOfNumbers;
+
+    }
+}
