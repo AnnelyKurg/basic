06a9b04fc818e8f46c57f941957ec4c0c457601d
Annely <annely.kurg@khk.ee>
2022-09-09 09:29:50 +0300

    week 4 - Exercise 72.3 Money transfers

--- week4/Ex72.java
+++ week4/Ex72.java
@@ -18,16 +18,38 @@ public class Ex72 {
         System.out.println(bartosAccount);
         System.out.println(bartosSwissAccount);
          */
-        Account mattAccount = new Account("Matt's account ", 1000);
+        Account mattAccount = new Account("Matt's account", 1000);
         Account myAccount = new Account("My account", 0);
+        Account a = new Account("A", 100);
+        Account b = new Account("B", 0);
+        Account c = new Account("C", 0);
 
         System.out.println(mattAccount);
         System.out.println(myAccount);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
 
-        mattAccount.withdrawal(100.00);
+        mattAccount.withdrawal(100.0);
         System.out.println(mattAccount);
+        transfer(a, b, 50);
 
         myAccount.deposit(100.0);
         System.out.println(myAccount);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        transfer(b, c, 25);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+
+    public static void  transfer(Account from, Account to, double amount) {
+        from.withdrawal(amount);
+        to.deposit(amount);
     }
 }
