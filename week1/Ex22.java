743dc6fb6df0a6578d85276fc1ebcb8999eee3b8
Annely <annely.kurg@khk.ee>
2022-09-16 18:36:26 +0300

    week 1 - Exercise 22.1

---
+++ week1/Ex22.java
@@ -0,0 +1,21 @@
+package week1;
+
+import java.util.Scanner;
+
+public class Ex22 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Type the password: ");
+            String password = reader.nextLine();
+            if (password.equals("carrot")) {
+                break;
+            } else {
+                System.out.println("Wrong!");
+            }
+        }
+
+        System.out.println("Right!");
+    }
+}
