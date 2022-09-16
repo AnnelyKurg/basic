3376757e41e7c7a7374ed09b85f77d02988b49a7
Annely <annely.kurg@khk.ee>
2022-09-16 17:43:44 +0300

    week 1 - Exercise 18 refactor

--- week1/Ex18.java
+++ week1/Ex18.java
@@ -9,15 +9,15 @@ public class Ex18 {
         int number = in.nextInt();
         if (number <= 29) {
             System.out.println("failed");
-        } else if (number ==30 || number <= 34){
+        } else if (number ==30 && number <= 34){
             System.out.println("Grade: 1");
-        } else if (number ==35 || number <= 39){
+        } else if (number ==35 && number <= 39){
             System.out.println("Grade: 2");
-        } else if (number ==40 || number <= 44){
+        } else if (number ==40 && number <= 44){
             System.out.println("Grade: 3");
-        } else if (number ==45 || number <= 49){
+        } else if (number ==45 && number <= 49){
             System.out.println("Grade: 4");
-        } else if (number ==50 || number <= 60){
+        } else if (number ==50 && number <= 60){
             System.out.println("Grade: 4");
         }
     }
