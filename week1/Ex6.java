a025fc78bba7f80c1446ec95c777fccdca92578b
Annely <annely.kurg@khk.ee>
2022-09-16 16:58:04 +0300

    week 1 - Exercise 6

---
+++ week1/Ex6.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        int x = 73457;
+        int y = 12888;
+
+        System.out.println(x+ " + " + y + " = " +(x+y));
+    }
+}
