2577ea402ae9a23056de5c7a09ae3fa3ea81ae10
Annely <annely.kurg@khk.ee>
2022-09-16 11:17:59 +0300

    week 7 - Exercise 4.4

--- week7/Suitcase.java
+++ week7/Suitcase.java
@@ -3,7 +3,7 @@ package week7;
 import java.util.ArrayList;
 
 public class Suitcase {
-    private ArrayList <Thing> things;
+    private ArrayList<Thing> things;
     private int weightLimit;
 
     public Suitcase(int weightLimit) {
@@ -12,25 +12,32 @@ public class Suitcase {
     }
 
     public void addThing(Thing thing){
-        int weightCurrent = 0;
-        for (Thing currentThing:this.things) {
-            weightCurrent += currentThing.getWeight();
-        }
-        if (weightCurrent + thing.getWeight() <= this.weightLimit) {
-        this.things.add(thing);
+        int weightCurrent = this.totalWeight();
+        if(weightCurrent + thing.getWeight() <= this.weightLimit) {
+            this.things.add(thing);
         }
     }
 
     @Override
     public String toString() {
+        int weightCurrent = this.totalWeight();
+        if(weightCurrent == 0) {
+            return "empty (" + weightCurrent + " kg)";
+        }
+        return this.things.size() + " things (" + weightCurrent + " kg)";
+    }
+
+    public void printThings(){
+        for (Thing thing :this.things) {
+            System.out.println(thing);
+        }
+    }
+
+    public int totalWeight(){
         int weightCurrent = 0;
-        for (Thing currentThing:this.things) {
+        for (Thing currentThing :this.things) {
             weightCurrent += currentThing.getWeight();
         }
-        if (weightCurrent ==0) {
-            return "empty ("+ weightCurrent + " kg)";
-        }
-        return this.things.size() + " things (" +
-                this.weightLimit + " kg)";
+        return weightCurrent;
     }
 }
