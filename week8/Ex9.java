cbf4ca7bc349abe33ae6d3655c0011a3f8d86bec
Annely <annely.kurg@khk.ee>
2023-08-20 18:03:39 +0300

    week 8 - Exercise 9.2: The Owner, Based of the Registration Plate

--- week8/Ex9.java
+++ week8/Ex9.java
@@ -9,28 +9,16 @@ public class Ex9 {
         RegistrationPlate reg2 = new RegistrationPlate("FI", "UXE-465");
         RegistrationPlate reg3 = new RegistrationPlate("D", "B WQ-431");
 
-        ArrayList<RegistrationPlate> finnish = new ArrayList<RegistrationPlate>();
-        finnish.add(reg1);
-        finnish.add(reg2);
+        VehicleRegister register = new VehicleRegister();
 
-        RegistrationPlate newPlate = new RegistrationPlate("FI", "ABC-123");
-        if (!finnish.contains(newPlate)) {
-            finnish.add(newPlate);
-        }
-        System.out.println("Finnish: " + finnish);
-        // if the equals method hasn't been overwritten, the same registration plate is repeated in the list
+        register.add(reg1, "Arto");
+        register.add(reg3, "Jügen");
 
-        HashMap<RegistrationPlate, String> owners = new HashMap<RegistrationPlate, String>();
-        owners.put(reg1, "Arto");
-        owners.put(reg3, "Jürgen");
+        System.out.println(register.get(reg1));
+        System.out.println(register.get(reg2));
+        System.out.println(register.get(reg3));
 
-//        System.out.println(reg1.hashCode());
-//        System.out.println(reg2.hashCode());
-//        System.out.println(reg3.hashCode());
-
-        System.out.println("owners:");
-        System.out.println(owners.get(new RegistrationPlate("FI", "ABC-123")));
-        System.out.println(owners.get(new RegistrationPlate("D", "B WQ-431")));
-        // if the hashCode hasn't been overwritten, the owners are not found
+        register.delete(reg2);
+        System.out.println(register.getOwners());
     }
 }
